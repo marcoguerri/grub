06e5150e170dbd6f921ff3607da1a8b230ed3bc8
Josef Bacik <jbacik@fb.com>
2015-08-07 11:24:27 -0700

    tcp: add window scaling and RTTM support
    Sometimes we have to provision boxes across regions, such as California to
    Sweden.  The http server has a 10 minute timeout, so if we can't get our 250mb
    image transferred fast enough our provisioning fails, which is not ideal.  So
    add tcp window scaling on open connections and set the window size to 1mb.  With
    this change we're able to get higher sustained transfers between regions and can
    transfer our image in well below 10 minutes.  Without this patch we'd time out
    every time halfway through the transfer.
    
    RTTM is needed in order to make window scaling work well under heavy congestion
    or packet loss.  In most cases grub could recover with just window scaling
    enabled, but on some machines the congestion would be so high that it would
    never recover and would timeout.
    
    I've made the window size configureable with the grub env variable
    "tcp_window_size".  By default this is set to 1mb but can be configured to
    whatever a user wants, and we will calculate the appropriate window size and
    scale settings.  Thanks,
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>

--- include/grub/net.h
+++ include/grub/net.h
@@ -574,6 +574,8 @@ grub_net_add_dns_server (const struct grub_net_network_level_address *s);
 void
 grub_net_remove_dns_server (const struct grub_net_network_level_address *s);
 
+void
+grub_net_tcp_init (void);
 
 extern char *grub_net_default_server;
 
